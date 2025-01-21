1c791a43a509813b086e5d72a07b300e572f2291
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-01-21 20:38:13 +0700

    box input

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 class Appnote(BoxLayout):
     pass
@@ -13,10 +14,9 @@ class Myapp(App):
     
     def add_activity(self, activity_text):
         if activity_text:
-            activity_layout = self.root.ids.activity
+            activity_layout = self.root.ids.activity_layout
             new_activity = BoxLayout(orientation='horizontal', size_hint=(1, None), height=40)
-            new_activity.add_widget(Label(text=activity_text, size_hint=(0.7, 1)))
-            new_activity.add_widget(TextInput(text='', size_hint=(0.3, 1)))
+            new_activity.add_widget(Label(text=activity_text, size_hint=(1, 1)))
             activity_layout.add_widget(new_activity)
             self.root.ids.activity_input.text = ''
     
